0a32364febf1f4f77bc0ca697d60af875b613921
Abhishek Sharma <sharmaash876@gmail.com>
2021-04-26 01:00:54 +0530

    new commit

--- public/js/placement.js
+++ public/js/placement.js
@@ -41,28 +41,28 @@ window.onload = function(){
         }
     });
 
-    var ctx2 = document.getElementById('pieChart').getContext('2d');
-    window.myPieChart = new Chart(ctx2,{
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: [10, 20, 30]
-            }],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-              ],
-            // These labels appear in the legend and in the tooltips when hovering different arcs
-            labels: [
-                'Red',
-                'Yellow',
-                'Blue'
-            ]
-        },
-        options:{
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
+    // var ctx2 = document.getElementById('pieChart').getContext('2d');
+    // window.myPieChart = new Chart(ctx2,{
+    //     type: 'pie',
+    //     data: {
+    //         datasets: [{
+    //             data: [10, 20, 30]
+    //         }],
+    //         backgroundColor: [
+    //             'rgb(255, 99, 132)',
+    //             'rgb(54, 162, 235)',
+    //             'rgb(255, 205, 86)'
+    //           ],
+    //         // These labels appear in the legend and in the tooltips when hovering different arcs
+    //         labels: [
+    //             'Red',
+    //             'Yellow',
+    //             'Blue'
+    //         ]
+    //     },
+    //     options:{
+    //         responsive: true,
+    //         maintainAspectRatio: false
+    //     }
+    // });
 }
